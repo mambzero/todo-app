64898a7475f54b89d3b05c09cb6270f7add2a39f
mambzero <mambzero@abv.bg>
2023-01-22 07:04:43 +0200

    Modified config and installed sass

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  pages: {
+    index: {
+      title: 'Todo App',
+      entry: 'src/main.js'
+    },
+  },
+  css: {
+    extract: process.env.NODE_ENV === 'production'
+  },
+  transpileDependencies: true,
 })
